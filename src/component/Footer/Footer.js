3338c4b2c499ee9760451d3c36647f7507c4c9be
Passionist59 <hiramjake59@gmail.com>
2022-03-20 14:32:23 +0200

    updated

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,9 +1,17 @@
 import React from "react";
-import { Container, Image } from 'react-bootstrap';
-import Button from '@mui/material/Button';
+import { Container } from 'react-bootstrap';
+import { ImTwitter } from "react-icons/im";
+import { BsTelegram } from "react-icons/bs";
 
 export default function Footer() {
     return(
-        <Container></Container>
+        <Container className="footer-container">
+            <p style={{fontSize:'70%'}}>LATEST BLOCK: 2891298  TERMS</p>
+            <div className="footericon-container">
+                <ImTwitter />
+                <BsTelegram />
+                <p style={{fontSize:'70%'}}>DOCS</p>
+            </div>
+        </Container>
     )
 }
