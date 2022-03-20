837feb46756ce61502bd199d4582c5e7b435c30f
Passionist59 <hiramjake59@gmail.com>
2022-03-20 13:30:09 +0200

    updated

--- src/Earn/Earn.js
+++ src/Earn/Earn.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Image } from 'react-bootstrap';
 import Button from '@mui/material/Button';
 
 export default function Earn() {
@@ -13,7 +13,7 @@ export default function Earn() {
             <div className='deposit-container'>
                 <h5 style={{fontWeight:'650', marginBottom:'0px'}}>TOTAL DEPOSIT</h5>
                 <h2>532,875 <span style={{fontSize:'18px', fontWeight:'650'}}>UST</span></h2>
-                <div className='deposit-withdraw-container'>
+                <div className='deposit-withdraw-container1'>
                     <Button className='deposit-btn'>Deposit</Button>
                     <Button className='withdraw-btn'>Withdraw</Button>
                 </div>
@@ -22,11 +22,33 @@ export default function Earn() {
                 {/* UST/LUNA container */}
             <div className='ust-luna-container'>
                 <div className='ust-container'>
-                    
+                    <Image src='' alt='UST' />
+                    <h2 style={{marginBottom:'20px'}}>UST</h2>
+                    <div className='apr-container'>
+                        <Button>APR</Button>
+                        <p>34.87%</p>
+                    </div>
+                    <hr style={{marginTop:'8%'}} />
+
+                    <div className='deposit-withdraw-container2'>
+                        <Button className='deposit-btn'>Deposit</Button>
+                        <Button className='withdraw-btn'>Withdraw</Button>
+                    </div>
                 </div>
 
                 <div className='luna-container'>
+                    <Image src='' alt='LUNA' />
+                    <h2 style={{marginBottom:'20px'}}>LUNA</h2>
+                    <div className='apr-container'>
+                        <Button>APR</Button>
+                        <p>14.87%</p>
+                    </div>
+                    <hr style={{marginTop:'8%'}} />
 
+                    <div className='deposit-withdraw-container2'>
+                        <Button className='deposit-btn'>Deposit</Button>
+                        <Button className='withdraw-btn'>Withdraw</Button>
+                    </div>
                 </div>
             </div>
             
